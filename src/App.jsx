bc956a2ac1c9d6e324042d3d630cd170ac0d941c
Lilyana Palikova <lilyanapalikova111@gmail.com>
2024-08-16 17:29:03 +0300

    add 2 import in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import { getUserData } from './services/user.service';
 import Header from './components/Header/Header';
 import Home from './views/Home/Home';
 import NotFound from './views/NotFound/NotFound';
+import Authenticated from './hoc/Authenticated';
+import Quizzes from './views/Quizzes/Quizzes';
+
 
 export default function App() {
     const [appState, setAppState] = useState({
@@ -40,6 +43,14 @@ export default function App() {
             >
                 <Header />
                 <Routes>
+                    <Route path="/search-results" element={<SearchResultPage />} />
+                    <Route path="/register" element={<RedirectIfAuthenticated><Register /></RedirectIfAuthenticated>} />
+                    <Route path="/login" element={<RedirectIfAuthenticated><Login /></RedirectIfAuthenticated>} />
+                    <Route path="/my-profile" element={<Authenticated><MyProfile /></Authenticated>} />
+                    <Route path="/quizzes" element={<Authenticated><Quizzes /></Authenticated>} />
+                    <Route path="/quiz-of-the-week" element={<Authenticated><QuizOfTheWeek /></Authenticated>} />
+                    <Route path="/ranking" element={<Authenticated><Ranking /></Authenticated>} />
+                    <Route path="/tournaments" element={<Authenticated><Tournaments /></Authenticated>} />
                     <Route path="/" element={<Home />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
